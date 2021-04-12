112edc22692bd8b73f5cecedaa029bec11f6d57b
Reet04 <reetagg111@gmail.com>
2021-04-12 13:05:51 +0530

    Selection sort completed

--- main.cpp
+++ main.cpp
@@ -5,3 +5,23 @@
 int main(int argc, char** argv) {
 	return 0;
 }
+
+//----------------------SELECTION SORT------------------------
+void selectionSort(int arr[], int n) 
+{ 
+    int min_pos; 
+  
+    for (int i=0;i<n-1;i++) 
+    { 
+        min_pos=i; 
+        for (int j=i+1;j<n;j++) 
+        {
+            if(arr[j]<arr[min_pos]) 
+                min_pos=j;
+        }
+        int temp=arr[i];
+        arr[i]=arr[min_pos];
+        arr[min_pos]=temp;
+    } 
+} 
+
