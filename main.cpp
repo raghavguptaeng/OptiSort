e92fc974e3d28ac453d75c4a8cc7543a1c87485b
Reet04 <reetagg111@gmail.com>
2021-04-12 13:08:00 +0530

    Merge Sort completed

--- main.cpp
+++ main.cpp
@@ -61,3 +61,52 @@ void insertionSort(int arr[], int n)
     } 
 } 
 
+
+//----------------------MERGE SORT------------------------
+void merge(int arr[],int l,int m, int r)
+{
+    int n1=m-l+1;
+    int n2=r-m;
+    int L[n1],R[n2];
+ 
+    for(int i=0;i<n1;i++)
+        L[i]=arr[l+i];
+    for(int j=0;j<n2;j++)
+        R[j]=arr[m+1+j];
+    int i = 0;
+    int j = 0;
+    int k = l;
+    while(i<n1 && j<n2) {
+        if(L[i]<=R[j]) {
+            arr[k]=L[i];
+            i++;
+        }
+        else {
+            arr[k]=R[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<n1) {
+        arr[k]=L[i];
+        i++;
+        k++;
+    }
+    while (j<n2) {
+        arr[k]=R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergesort(int arr[],int l,int r)
+{
+    if(l<r)
+    {
+        int mid=(r+l)/2;
+        mergesort(arr,l,mid);
+        mergesort(arr,mid+1,r);
+        merge(arr,l,mid,r);
+    }
+}
+
