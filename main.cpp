b9ed580b86024b8cec3d4960e5d3cb58c84cc60c
Reet04 <reetagg111@gmail.com>
2021-04-12 13:07:11 +0530

    Insertion Sort completed

--- main.cpp
+++ main.cpp
@@ -42,3 +42,22 @@ void bubbleSort(int arr[], int n)
         }
     } 
 } 
+
+
+//----------------------INSERTION SORT------------------------
+void insertionSort(int arr[], int n) 
+{ 
+    int key,j;
+    for(int i=1;i<n;i++)
+    {
+        key = arr[i];
+        j=i-1;
+        while(j>=0&&arr[j]>key)
+        {
+            arr[j+1]=arr[j];
+            j=j-1;
+        }
+        arr[j+1]=key;
+    } 
+} 
+
