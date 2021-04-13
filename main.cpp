6cb16b4804134b268e3cbc89d67bebd2cd66b44a
Raghav Gupta <E19CSE258@bennett.edu.in>
2021-04-13 15:28:22 +0530

    Merge sort made multithreaded

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char** argv) {
-	return 0;
-}
-
+#include <iostream>
+#include <pthread.h>
+#include <time.h>  
+// number of threads
+#define THREAD_MAX 4
 //----------------------SELECTION SORT------------------------
 void selectionSort(int arr[], int n) 
 { 
@@ -63,54 +61,90 @@ void insertionSort(int arr[], int n)
 
 
 //----------------------MERGE SORT------------------------
-void merge(int arr[],int l,int m, int r)
+void merge(int low, int mid, int high , int a[] , int n)
 {
-    int n1=m-l+1;
-    int n2=r-m;
-    int L[n1],R[n2];
- 
-    for(int i=0;i<n1;i++)
-        L[i]=arr[l+i];
-    for(int j=0;j<n2;j++)
-        R[j]=arr[m+1+j];
-    int i = 0;
-    int j = 0;
-    int k = l;
-    while(i<n1 && j<n2) {
-        if(L[i]<=R[j]) {
-            arr[k]=L[i];
-            i++;
-        }
-        else {
-            arr[k]=R[j];
-            j++;
-        }
-        k++;
+    int* left = new int[mid - low + 1];
+    int* right = new int[high - mid];
+  
+    // n1 is size of left part and n2 is size
+    // of right part
+    int n1 = mid - low + 1, n2 = high - mid, i, j;
+  
+    // storing values in left part
+    for (i = 0; i < n1; i++)
+        left[i] = a[i + low];
+  
+    // storing values in right part
+    for (i = 0; i < n2; i++)
+        right[i] = a[i + mid + 1];
+  
+    int k = low;
+    i = j = 0;
+  
+    // merge left and right in ascending order
+    while (i < n1 && j < n2) {
+        if (left[i] <= right[j])
+            a[k++] = left[i++];
+        else
+            a[k++] = right[j++];
     }
-    while(i<n1) {
-        arr[k]=L[i];
-        i++;
-        k++;
+  
+    // insert remaining values from left
+    while (i < n1) {
+        a[k++] = left[i++];
     }
-    while (j<n2) {
-        arr[k]=R[j];
-        j++;
-        k++;
+  
+    // insert remaining values from right
+    while (j < n2) {
+        a[k++] = right[j++];
+    }
+}  
+// merge sort function
+void merge_sort(int low, int high,int arr[] , int n)
+{
+    // calculating mid point of array
+    int mid = low + (high - low) / 2;
+    if (low < high) {
+
+        merge_sort(low, mid,arr,n);
+        merge_sort(mid + 1, high,arr,n);
+        merge(low, mid, high,arr,n);
     }
 }
-
-void mergesort(int arr[],int l,int r)
+void* merge_sort(int arr[] , int n,void* arg)
 {
-    if(l<r)
-    {
-        int mid=(r+l)/2;
-        mergesort(arr,l,mid);
-        mergesort(arr,mid+1,r);
-        merge(arr,l,mid,r);
+    // which part out of 4 parts
+    int thread_part = part++;
+  
+    // calculating low and high
+    int low = thread_part * (MAX / 4);
+    int high = (thread_part + 1) * (MAX / 4) - 1;
+  
+    // evaluating mid point
+    int mid = low + (high - low) / 2;
+    if (low < high) {
+        merge_sort(low, mid,arr,n);
+        merge_sort(mid + 1, high,arr,n);
+        merge(low, mid, high,arr,n);
     }
 }
-
-
+void mergeSort(int arr[] , int n){
+    pthread_t threads[THREAD_MAX];
+  
+    // creating 4 threads
+    for (int i = 0; i < THREAD_MAX; i++)
+        pthread_create(&threads[i], NULL, merge_sort(arr,n)  ,
+                                        (void*)NULL);
+  
+    // joining all 4 threads
+    for (int i = 0; i < 4; i++)
+        pthread_join(threads[i], NULL);
+  
+    // merging the final 4 parts
+    merge(0, (MAX / 2 - 1) / 2, MAX / 2 - 1);
+    merge(MAX / 2, MAX/2 + (MAX-1-MAX/2)/2, MAX - 1);
+    merge(0, (MAX - 1)/2, MAX - 1);
+}
 //----------------------QUICK SORT------------------------
 int partition(int arr[],int low,int high)
 {
@@ -138,5 +172,18 @@ void quickSort(int arr[], int low, int high)
         int pi = partition(arr, low, high);
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
+    }
+}
+// Driver Ccodes to come here;
+int main() {
+	int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0 ; i<n ; ++i){
+        cin>>arr[i];
+    }
+    mergeSort(arr,n);
+    for(int i=0 ; i<n ; ++i){
+        cout<<arr[i]<<" ";
     }
 }
